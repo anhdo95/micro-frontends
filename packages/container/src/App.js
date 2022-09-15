e953ae56d362206429a147f12043642a41c98f05
Anh Do <do.dinh.anh@linecorp.com>
2022-09-15 23:03:56 +0700

    Render marketing app on the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 
+import MarketingApp from './components/MarketingApp'
+
 export default () => {
   return (
     <div>
       <StylesProvider>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/pricing" component={() => <div></div>} />
-            <Route path="/" component={() => <div></div>} />
+            <Route exact path="/auth" component={() => <></>} />
+            <Route path="/" component={MarketingApp} />
           </Switch>
         </BrowserRouter>
       </StylesProvider>
